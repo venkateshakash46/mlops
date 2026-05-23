7ca54d71698f37ad0c5b37f369db828b86e10c2d
venkateshakash46 <venkateshakash46@gmail.com>
2026-05-23 23:47:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello from FastAPI"}
+    return {"message": "Hello from Render FastAPI"}
+
+@app.post("/predict")
+def predict(data: dict):
+    return {
+        "prediction": "setosa",
+        "received_data": data
+    }
